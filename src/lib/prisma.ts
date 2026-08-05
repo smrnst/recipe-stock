c9fada0bb02c263acd0e4153978247f201c38a2b
smrnst <4656983+smrnst@users.noreply.github.com>
2026-08-05 21:29:43 +0900

    本番用に環境変数を追加

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from "../generated/prisma/client";
 
 const adapter = new PrismaLibSql({
   url: process.env.DATABASE_URL ?? "file:./dev.db",
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
